1d47da108d32db88abaf7d7a7e3697fe343ca7da
Ankush <obviouslyankush@gmail.com>
2023-12-29 21:56:07 +0530

    made a component for nav bar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+import Navbar from './assets/components/Navbar.jsx'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar/>
+    </>
+  
+  )
+}
+
+export default App
