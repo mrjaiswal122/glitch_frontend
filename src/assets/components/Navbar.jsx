1f92d16ca87684b92bb3e96221ac3911396cb244
Ankush <obviouslyankush@gmail.com>
2023-12-29 22:57:55 +0530

    started implementing hamburger menu

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
         className="bg-[#9f9fce] flex  max-w-full  items-center text-black h-20  ">
 
 
-        <div id="logo" className="ml-[2px] h-24 w-24 overflow-hidden svg-container bg-no-repeat bg-center bg-contain scale-[1.4]">
+        <div id="logo" className="ml-[2px] h-24 w-24 overflow-hidden logo bg-no-repeat bg-center bg-contain scale-[1.3]">
         </div>
 
 
@@ -31,16 +31,7 @@ function Navbar() {
         </div>
 
 
-        <div id="ham" className="sm:hidden ml-auto ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 64 64"
-            width="48px"
-            height="48px"
-          >
-            <path d="M51 46c1.104 0 2 .895 2 2 0 1.105-.896 2-2 2-.601 0-37.399 0-38 0-1.104 0-2-.895-2-2 0-1.105.896-2 2-2C13.601 46 50.399 46 51 46zM51 30c1.104 0 2 .895 2 2 0 1.105-.896 2-2 2-.601 0-37.399 0-38 0-1.104 0-2-.895-2-2 0-1.105.896-2 2-2C13.601 30 50.399 30 51 30zM51 14c1.104 0 2 .895 2 2 0 1.105-.896 2-2 2-.601 0-37.399 0-38 0-1.104 0-2-.895-2-2 0-1.105.896-2 2-2C13.601 14 50.399 14 51 14z" />
-          </svg>
-        </div>
+        <div id="ham" className="sm:hidden ml-auto ham h-12 w-12"></div>
       </nav>
       <script>
         
